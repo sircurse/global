dba7070ab3dd441706113aa99d943e420de97025
sircurse <anderson@lalumail.com>
2022-04-28 13:13:47 +0200

    Update curseria.lua

--- data/scripts/talkactions/player/curseria.lua
+++ data/scripts/talkactions/player/curseria.lua
@@ -7,7 +7,7 @@ function twitchTV.onSay(player, words, param)
 	.. "\nhttps://discord.gg/y6C3BUrh4u" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_SWORD), configRateSkill)
 	.. "\n " .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_CLUB), configRateSkill)
 	.. "\nTwitch:" .. getRateFromTable(skillsStages, player:getSkillLevel(SKILL_AXE), configRateSkill)
-	.. "\nhttps://www.twitch.tv/curseofcourse"
+	.. "\nhttps://www.twitch.tv/curseofcourse")
 	return false
 end
 
