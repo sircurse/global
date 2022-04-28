e9a619b13ce9d74ba5638d556d4cb39b699e615d
sircurse <anderson@lalumail.com>
2022-04-29 00:01:29 +0200

    Sync to local repo
    Sync to local repo

---
+++ bkp-custom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#cp config.lua custom/
+#cp data/stages.lua custom/
+#cp data/scripts/globalevents/customs/save_interval.lua custom/
+#cp data/scripts/globalevents/others/online_record.lua custom/
+#cp data/scripts/creaturescripts/others/login.lua custom/
+#cp data/scripts/creaturescripts/others/player_death.lua custom/
+cp data/scripts/custom/discord_specialwebhook.lua custom/
+
