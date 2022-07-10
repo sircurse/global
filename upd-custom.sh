41dba869b15dc307541406f5ee2dfb3495d6615f
sircurse <anderson@lalumail.com>
2022-07-10 21:07:27 +0200

    Removed unnecessary scripts
    Removed unnecessary scripts

--- upd-custom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#cp custom/config.lua .
-#cp custom/stages.lua data/
-#cp custom/save_interval.lua data/scripts/globalevents/customs/
-#cp custom/online_record.lua data/scripts/globalevents/others/
-#cp custom/login.lua data/scripts/creaturescripts/others/
-#cp custom/player_death.lua data/scripts/creaturescripts/
-cp custom/discord_specialwebhook.lua data/scripts/custom/
-
